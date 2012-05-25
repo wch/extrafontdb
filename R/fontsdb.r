6e9caa95da7c204d05116c414ffab22f54ef2655
Winston Chang <winston@stdout.org>
2012-05-25 00:48:39 -0500

    Added code

---
+++ R/fontsdb.r
@@ -0,0 +1,11 @@
+#' fonts package
+#'
+#' Ths package is meant to be used with the fonts package. The
+#' fonts package contains the code to install and use fonts, while this
+#' package contains the directory structure to hold the fonts database.
+#' See the \link{fonts} for more information.
+#'
+#' @name fontsdb
+#' @docType package
+#' @aliases fontsdb package-fontsdb
+NULL
