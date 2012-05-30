de714c20d6fb40e9adb520989151d51923db13ae
Winston Chang <winston@stdout.org>
2012-05-29 22:58:29 -0500

    Update help

--- R/fontsdb.r
+++ R/fontsdb.r
@@ -3,7 +3,8 @@
 #' Ths package is meant to be used with the fonts package. The
 #' fonts package contains the code to install and use fonts, while this
 #' package contains the directory structure to hold the fonts database.
-#' See the \link{fonts} for more information.
+#' See the \url{https://github.com/wch/fontsdb} and
+#' \url{https://github.com/wch/fonts} for more information.
 #'
 #' @name fontsdb
 #' @docType package
